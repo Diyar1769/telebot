23d8e8878e4969adc9dd5fc54a6a6d563d9b53df
Diyar1769 <diyar1609@mail.ru>
2025-02-07 22:04:09 +0500

    second

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-a = 5
-print(a)
-b = 2
-print(b)
+# a = [1,2,3,4,5,6,7,8,9,10]
+# sum1 = 0
+# for i in a:
+#     sum1 += i
+# print(sum1)
+
+# a = [1,2,3,4,5,6,7,8,9,10]
+# for i in a:
+#     if i%2==0:
+#         print(i)
+
+# a = [1,2,3,4,5,6,7,8,9,10]
+# max = max(a)
+# print(max)
+
+# a = [1,2,3,4,5,6,7,8,9,10]
+# for i in reversed(a):
+#     print(i)
+
+# a = 5
+# for i in range(1,11):
+#     print(a * i)
